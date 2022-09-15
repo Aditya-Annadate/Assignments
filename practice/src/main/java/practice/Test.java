e88e0fc235748c775b6d5d65647ab5fdf91cc30e
aditya.annadate <aditya.annadate@YPH1016302LT.Yash.com>
2022-09-15 14:49:22 +0530

    First Commit from eclipse

---
+++ practice/src/main/java/practice/Test.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		
+		String s=new String("payal");
+		System.out.println(s.hashCode());
+		String s1="payal";
+		System.out.println(s1.hashCode());
+		
+		
+	}
+
+}
